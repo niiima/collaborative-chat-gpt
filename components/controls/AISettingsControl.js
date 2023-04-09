130a10d793a8ef227f70e55d87fb050883618989
niiima <nima.bastani@gmail.com>
2023-04-09 19:27:02 +0200

    Update logos both for gpt and user

--- components/controls/AISettingsControl.js
+++ components/controls/AISettingsControl.js
@@ -3,40 +3,19 @@ import { useControls } from "leva";
 import AIContext from "../../context/AIContext";
 import ColorfulButtonSet from "../Buttons/ColorfulButtons";
 
-const engines = [
-  {
-    type: "classic",
-    engines: [
-      "text-davinci-003",
-      "text-curie-001",
-      "text-babbage-001",
-      "text-ada-001",
-    ],
-  },
-  {
-    type: "new",
-    engines: ["gpt-3.5-turbo", "gpt-3.5-turbo-0301"],
-  },
-];
 export default function ChatSettingsControl({ aiType = "basic" }) {
   const { AIstate, setAIState, activeEngine } = useContext(AIContext);
 
   const {
-    // engine,
     max_tokens,
     temperature,
     top_p,
     frequency_penalty,
     presence_penalty,
   } = useControls({
-    // engine: {
-    //   options: engines.find((engine) => engine.type === aiType).engines,
-    //   onChange: (value) => setAIState({ engine: value }),
-    // },
     max_tokens: {
-      value: 150, //2048,
       step: 1,
-      max: 4000, //activeEngine.maxTokens, //2048,
+      max: 4000,
       min: 0,
       onChange: (value) => setAIState({ max_tokens: value }),
     },
