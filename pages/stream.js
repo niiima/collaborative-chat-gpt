2e4ed185cf078327e5aa7eaafcb3cfe6138f4600
niiima <nima.bastani@gmail.com>
2023-04-12 23:36:50 +0200

    Fix active engine selection in other routs

--- pages/stream.js
+++ pages/stream.js
@@ -154,7 +154,11 @@ export default function MyPage() {
         <GroupRadioButtons
           items={[
             ...engines.map((engine) => {
-              return { text: engine.name, value: engine.key };
+              return {
+                text: engine.name,
+                value: engine.key,
+                isActive: engine.key === activeEngine.key,
+              };
             }),
           ]}
           changeHandler={(e) =>
