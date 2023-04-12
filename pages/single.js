f4fa1c8e7e7535ca55358453a3cb735e9c0d6861
niiima <nima.bastani@gmail.com>
2023-04-12 14:41:40 +0200

    Add single conversation page

---
+++ pages/single.js
@@ -0,0 +1,205 @@
+import { useState, useContext } from "react";
+import Head from "next/head";
+import { experimentalEngines as engines } from "../model/model.js";
+import ChatContext from "../context/ChatContext.js";
+import AIContext from "../context/AIContext.js";
+import Sidebar from "../components/Sidebar/Sidebar.js";
+import ChatSettingsControl from "../components/controls/AISettingsControl";
+import Header from "../components/Header/Header.js";
+import UIContext from "../context/UIContext.js";
+import { FlexItem } from "../components/Atoms/FlexItem.js";
+import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
+import { Flex } from "../components/Atoms/Flex";
+import OrdinaryButton from "../components/Buttons/OrdinaryButton";
+import { MdDeleteSweep } from "react-icons/md";
+import { GiStopSign } from "react-icons/gi";
+import dynamic from "next/dynamic";
+const ChatComponent = dynamic(() => import("../components/ChatComponent"), {
+  loading: () => (
+    <div
+      style={{ position: "absolute", left: "43%", top: "48%" }}
+      className='loading-spinner'></div>
+  ),
+});
+const ActSelector = dynamic(
+  () => import("../components/AIManipulatingComponents/ActSelector.js"),
+  {
+    loading: () => (
+      <div
+        style={{ position: "absolute", left: "43%", top: "48%" }}
+        className='loading-spinner'></div>
+    ),
+  }
+);
+const StopGeneratePromptButton = styled(OrdinaryButton)`
+  /* color: red; */
+`;
+
+export default function MyPage() {
+  const { asideExpanded, setAsideExpand } = useContext(UIContext);
+
+  const {
+    chatHistory,
+    addToHistory,
+    isLoading,
+    setIsLoading,
+    clearChatHistory,
+  } = useContext(ChatContext);
+
+  const { AIstate, setAIState } = useContext(AIContext);
+  const [activeEngine, setActiveEngine] = useState(engines[0]);
+
+  const [prompt, setPrompt] = useState("");
+  const [systemPrompt, setSystemPrompt] = useState(
+    `Assist user for what they might ask, get involve in the conversation and try to provide accurate answers for their questions.`
+  );
+  const [stream, setStream] = useState("");
+
+  async function handleSubmit(e) {
+    const streamTextArray = [];
+    const prompt_timestamp = new Date();
+    setIsLoading(true);
+
+    try {
+      const messages = [{ role: "system", content: prompt }];
+
+      let options = {
+        engine: activeEngine.key,
+        messages: messages,
+        ...AIstate,
+      };
+
+      const response = await fetch("/api/generate-chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(options),
+      });
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const data = response.body;
+      if (!data) {
+        return;
+      }
+
+      const reader = data.getReader();
+      const decoder = new TextDecoder();
+      let done = false;
+
+      let init = false;
+      while (!done) {
+        const { value, done: doneReading } = await reader.read();
+        done = doneReading;
+        const chunkValue = decoder.decode(value);
+        if (init === false) {
+          init = true;
+          const substr = chunkValue.slice(9); // remove assistant from the beginning of conversation
+          setStream((prev) => prev + substr);
+          streamTextArray.push(substr);
+        } else {
+          setStream((prev) => prev + chunkValue);
+          streamTextArray.push(chunkValue);
+        }
+      }
+
+      const completion_timestamp = new Date();
+      const completion = streamTextArray.join("");
+
+      const tokenCountResult = await fetch("/api/calculate-tokens", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt, completion }),
+      });
+
+      const { prompt_tokens, completion_tokens } =
+        await tokenCountResult.json();
+
+      addToHistory({
+        chatId: uuidv4(),
+        prompt: prompt,
+        prompt_timestamp: prompt_timestamp,
+        completion: streamTextArray.join(""),
+        completion_timestamp: completion_timestamp,
+        engine: activeEngine.key,
+        prompt_tokens: prompt_tokens,
+        completion_tokens: completion_tokens,
+        prompt_price: (
+          (prompt_tokens / 1000) *
+          activeEngine.costPerKiloToken
+        ).toFixed(5),
+        completion_price: (
+          (completion_tokens / 1000) *
+          activeEngine.costPerKiloToken
+        ).toFixed(5),
+      });
+      //console.log(chatHistory);
+      setStream("");
+      setPrompt("");
+    } catch {
+      (err) => console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function handlePromptChange(e) {
+    let trimPrompt = e.trim();
+    if (trimPrompt.length > 0) {
+      setPrompt(trimPrompt);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>GPT only knows your last question</title>
+      </Head>
+      <Sidebar show={asideExpanded}>
+        <ChatSettingsControl aiType='new' />
+      </Sidebar>
+      <Header>
+        <Flex>
+          <FlexItem>
+            <OrdinaryButton
+              text={""}
+              icon={<MdDeleteSweep size='20' color='orange' />}
+              handleOnClick={() => clearChatHistory()}></OrdinaryButton>
+            {/* <StopGeneratePromptButton
+            text={""}
+            icon={<GiStopSign size='30' />}
+            handleOnClick={() => clearChatHistory()}
+          /> */}
+          </FlexItem>
+          <FlexItem>
+            Models:{" "}
+            <select
+              onChange={(e) => {
+                let engineType = e.currentTarget.value;
+                // console.log(engineType);
+                setActiveEngine(engines.find((eng) => eng.key === engineType));
+              }}>
+              {engines.map((eng) => (
+                <option key={eng.id} value={eng.key}>
+                  {eng.name}
+                </option>
+              ))}
+            </select>
+          </FlexItem>
+        </Flex>
+      </Header>
+      <ChatComponent
+        stream={stream}
+        prompt={prompt}
+        handleSendMessage={handleSubmit}
+        handlePromptTextChange={handlePromptChange}
+        handleOnClick={() => setAsideExpand(false)}
+      />
+    </div>
+  );
+}
