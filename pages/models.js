c63c16dc9542bc88021a0a074e7135d7029f8179
niiima <nima.bastani@gmail.com>
2023-04-16 16:49:55 +0200

    Remove unused import of uuid

--- pages/models.js
+++ pages/models.js
@@ -7,7 +7,7 @@ import Sidebar from "../components/Sidebar/Sidebar.js";
 import ChatSettingsControl from "../components/AIManipulatingComponents/AISettingsControl.js";
 import Header from "../components/Header/Header.js";
 import UIContext from "../context/UIContext.js";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import ColorfulButtonSet from "../components/Buttons/ColorfulButtons.js";
 import GroupRadioButtons from "../components/Inputs/GroupRadio/GroupRadioButtons.js";
 //import EngineSelector from "../components/AIManipulatingComponents/EngineSelector.js";
@@ -26,188 +26,6 @@ export default function MyPage() {
   const [activeEngine, setActiveEngine] = useState(engines[0]);
   const [models, setModels] = useState([]);
 
-  // function hierarchyStructure(data) {
-  //   // Create a dictionary with owned_by as keys and an empty children array as values
-  //   const hierarchy = {};
-  //   for (let model of data) {
-  //     const parentId = model.owned_by || null;
-  //     hierarchy[parentId] = hierarchy[parentId] || { children: [] };
-  //   }
-
-  //   // Add each model to its parent's children array
-  //   for (let model of data) {
-  //     const parentId = model.owned_by || null;
-  //     const parent = hierarchy[parentId];
-  //     const child = {
-  //       created: model.created,
-  //       id: model.id,
-  //       object: model.object,
-  //       children: [],
-  //     };
-  //     // Sort children based on created field
-  //     const children = parent.children;
-  //     const index = children.findIndex((c) => c.created < child.created);
-  //     if (index === -1) {
-  //       children.push(child);
-  //     } else {
-  //       children.splice(index, 0, child);
-  //     }
-  //   }
-
-  //   // Remove permission property from the result
-  //   const cleanHierarchy = (node) => {
-  //     delete node.permission;
-  //     for (let child of node.children) {
-  //       cleanHierarchy(child);
-  //     }
-  //   };
-  //   for (let parent of Object.values(hierarchy)) {
-  //     cleanHierarchy(parent);
-  //   }
-
-  //   return hierarchy[null].children; // Return the root nodes (models without parent)
-  // }
-
-  function hierarchyStructure(data) {
-    // Create a dictionary with owned_by as keys and an empty children array as values
-    const hierarchy = {};
-    for (let model of data) {
-      const parentId = model.owned_by || null;
-      hierarchy[parentId] = hierarchy[parentId] || { children: [] };
-    }
-
-    // Add each model to its parent's children array
-    for (let model of data) {
-      const parentId = model.owned_by || null;
-      const parent = hierarchy[parentId];
-      const child = {
-        created: model.created,
-        id: model.id,
-        object: model.object,
-        children: [],
-      };
-      // Sort children based on created field
-      const children = parent.children;
-      const index = children.findIndex((c) => c.created < child.created);
-      if (index === -1) {
-        children.push(child);
-      } else {
-        children.splice(index, 0, child);
-      }
-    }
-
-    // Remove permission property from the result
-    const cleanHierarchy = (node) => {
-      delete node.permission;
-      for (let child of node.children) {
-        cleanHierarchy(child);
-      }
-    };
-    for (let parent of Object.values(hierarchy)) {
-      cleanHierarchy(parent);
-    }
-
-    // Find the root nodes (models without parent) based on parents that don't exist in hierarchy
-    const rootNodes = [];
-    for (let model of data) {
-      if (!hierarchy[model.id]) {
-        rootNodes.push({
-          created: model.created,
-          id: model.id,
-          object: model.object,
-          children: [],
-        });
-      }
-    }
-    console.log(rootNodes);
-
-    // Return the root nodes with their children sorted by created timestamp and permission property removed
-    for (let parent of rootNodes) {
-      parent.children = hierarchy[parent.id].children;
-    }
-    return rootNodes;
-  }
-
-  function getModelPurposes(modelName) {
-    const purposes = [];
-
-    if (modelName.includes("edit") || modelName.includes("code")) {
-      purposes.push("edit_code");
-    }
-
-    if (modelName.includes("search") || modelName.includes("query")) {
-      purposes.push("search");
-    }
-
-    if (modelName.includes("similarity")) {
-      purposes.push("text_similarity");
-    }
-
-    if (modelName.includes("instruct")) {
-      purposes.push("instruction");
-    }
-
-    if (modelName.includes("turbo")) {
-      purposes.push("turbo");
-    }
-
-    if (modelName.includes("whisper")) {
-      purposes.push("voice");
-    }
-
-    if (modelName.includes("embedding")) {
-      purposes.push("text_embedding");
-    }
-
-    if (modelName.includes("gpt-3.5")) {
-      purposes.push("advanced_generation");
-    }
-
-    if (modelName.includes("cushman")) {
-      purposes.push("cushman");
-    }
-
-    if (modelName.includes("if-")) {
-      purposes.push("integrated");
-    }
-
-    if (modelName.includes("ada")) {
-      purposes.push("text_generation");
-    }
-
-    if (modelName.includes("babbage")) {
-      purposes.push("general_purpose");
-    }
-
-    if (modelName.includes("curie")) {
-      purposes.push("text_generation");
-    }
-
-    if (modelName.includes("davinci")) {
-      purposes.push("general_purpose");
-    }
-
-    return purposes;
-  }
-
-  function flattenAndSort(data) {
-    const newData = data.map(({ permission, created, ...model }) => {
-      console.log(model.id);
-      const purposes = getModelPurposes(model.id);
-      console.log(purposes);
-      // If `permission` has a `created` property, move the other properties to `model`
-      if (permission && permission.created) {
-        model = { ...model, purposes: purposes, ...permission };
-      }
-      return { ...model, purposes };
-    });
-
-    // Sort the new data by the `created` property
-    newData.sort((a, b) => b.created - a.created);
-
-    return newData;
-  }
-
   async function getModels() {
     const response = await fetch("/api/models", {
       method: "GET",
@@ -220,16 +38,11 @@ export default function MyPage() {
       throw new Error(response.statusText);
     }
 
-    const { data } = await response.json();
-    // const hierarchy = hierarchyStructure(data);
-    // console.log(hierarchy);
-    const flattenedAndSorted = flattenAndSort(data);
-    console.log(flattenedAndSorted);
-    // console.log(data);
-    // console.log(JSON.stringify(data.map((d) => d.id)));
-    if (!data) {
+    const res = await response.json();
+    //console.log(res);
+    if (!res.data) {
       return;
-    } else setModels(flattenedAndSorted);
+    } else setModels(res.data);
   }
 
   // useEffect(() => getModels(), []);
