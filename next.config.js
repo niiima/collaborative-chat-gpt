3af8150f86bc513260beeca77a0826e3c541eeaf
niiima <nima.bastani@gmail.com>
2023-05-09 17:20:43 +0200

    Unable app directory and Fix bug in get-insert api

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-// next.config.js
 module.exports = {
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
-  experimental: { appDir: true },
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    return config;
-  },
+  // experimental: { appDir: true },
+  // webpack(config) {
+  //   config.experiments = { ...config.experiments, topLevelAwait: true };
+  //   return config;
+  // },
 };
